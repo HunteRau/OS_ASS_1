6fa49e04225fe8c1e47208f7a9beb3a5a8699af1
ColdAnkles <norealname8@hotmail.com>
2015-08-19 16:46:16 +0930

    Added Basic Classes

---
+++ src/SchedulerDriver/Request.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SchedulerDriver;
+
+/**
+ *
+ * @author Hayden
+ */
+
+public class Request {
+    public int agentID;
+    public reqType rORc;
+    public seatType type;
+    public int seatNum;
+    
+}
