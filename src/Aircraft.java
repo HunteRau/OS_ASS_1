54acd7355db37c7a99293f2be7a1dbf8463e46d3
ColdAnkles <norealname8@hotmail.com>
2015-08-26 19:37:03 +0930

    Fix'd Errors
    All errors associated with misspellings and incorrect use of enums are
    corrected.

--- src/Aircraft.java
+++ src/Aircraft.java
@@ -1,86 +1,86 @@
 class Aircraft {
 	int firstClassSeats;
 	int businessClassSeats;
-	int echoClassSeats;
+	int ecoClassSeats;
 	
 	int firstClassSeatsFilled;
 	int businessClassSeatsFilled;
-	int echoClassFilled;
+	int ecoClassSeatsFilled;
 	
 	public Aircraft(int numFirstClass, int numBusinessClass, int numEcoClass) {
 		firstClassSeats = numFirstClass;
 		businessClassSeats = numBusinessClass;
-		echoClassSeats = numEcoClass;
+		ecoClassSeats = numEcoClass;
 	}
 	
-	boolean reserve(SeatType type) {
-		if (type == FIRST) {
+	boolean reserve(seatType type) {
+		if (type == seatType.FIRST) {
 			if (firstClassSeatsFilled < firstClassSeats) {
 				firstClassSeats++;
 				return true;
 			}								
-		} else if (type == BUSINESS) {
+		} else if (type == seatType.BUSINESS) {
 			if (businessClassSeatsFilled < businessClassSeats) {
 				businessClassSeats++;
 				return true;
 			}			
-		} else if (type == ECONOMY) {
-			if (echoClassSeatsFilled < echoClassSeats) {
-				echoClassSeats++;
+		} else if (type == seatType.ECONOMY) {
+			if (ecoClassSeatsFilled < ecoClassSeats) {
+				ecoClassSeats++;
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	boolean reserve(SeatType type, int numOfSeats) {
-		if (type == FIRST) {
+	boolean reserve(seatType type, int numOfSeats) {
+		if (type == seatType.FIRST) {
 			if (firstClassSeatsFilled + numOfSeats <= firstClassSeats) {
 				firstClassSeats = numOfSeats + firstClassSeats;
 				return true;
 			}								
-		} else if (type == BUSINESS) {
+		} else if (type == seatType.BUSINESS) {
 			if (businessClassSeatsFilled + numOfSeats <= businessClassSeats) {
 				businessClassSeats = numOfSeats + businessClassSeats;
 				return true;
 			}			
-		} else if (type == ECONOMY) {
-			if (echoClassSeatsFilled + numOfSeats <= echoClassSeats) {
-				echoClassSeats = numOfSeats + echoClassSeats;
+		} else if (type == seatType.ECONOMY) {
+			if (ecoClassSeatsFilled + numOfSeats <= ecoClassSeats) {
+				ecoClassSeats = numOfSeats + ecoClassSeats;
 				return true;
 			}
 		}
 		return false;	
 	}
 	
-	void cancel(SeatType type) {
-		if (type == FIRST) {
+	void cancel(seatType type) {
+		if (type == seatType.FIRST) {
 			if (firstClassSeatsFilled > 0) {
 				firstClassSeatsFilled--;
 			}								
-		} else if (type == BUSINESS) {
+		} else if (type == seatType.BUSINESS) {
 			if (businessClassSeatsFilled > 0) {
 				businessClassSeatsFilled--;
 			}			
-		} else if (type == ECONOMY) {
-			if (echoClassSeatsFilled > 0) {
-				echoClassSeatsFilled--;
+		} else if (type == seatType.ECONOMY) {
+			if (ecoClassSeatsFilled > 0) {
+				ecoClassSeatsFilled--;
 			}
 		}
 	}
 	
-	void cancel(SeatType type, int numOfSeats) {
-		if (type == FIRST) {
-			if (firstClassSeatsFilled - numOfSeats < 0) {
+	void cancel(seatType type, int numOfSeats) {
+		if (type == seatType.FIRST) {
+			if (firstClassSeatsFilled - numOfSeats >= 0) {
 				firstClassSeatsFilled--;
 			}								
-		} else if (type == BUSINESS) {
-			if (businessClassSeatsFilled - numOfSeats < 0) {
+		} else if (type == seatType.BUSINESS) {
+			if (businessClassSeatsFilled - numOfSeats >= 0) {
 				businessClassSeatsFilled--;
 			}			
-		} else if (type == ECONOMY) {
-			if (echoClassSeatsFilled - numOfSeats < 0) {
-				echoClassSeatsFilled--;
+		} else if (type == seatType.ECONOMY) {
+			if (ecoClassSeatsFilled - numOfSeats >= 0) {
+				ecoClassSeatsFilled--;
 			}
 		}
 	}
