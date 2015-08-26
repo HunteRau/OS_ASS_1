d1de46e3a8cffa75f8f6c9c3280ddc357471bac4
ColdAnkles <norealname8@hotmail.com>
2015-08-26 19:05:24 +0930

    Moved
    Moved Files from SchedulerDriver package to default package

---
+++ src/Request.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Hayden
+ */
+
+public class Request {
+    public String agent;
+    public reqType rORc;
+    public seatType type;
+    public int seatNum;
+    public int arrivalTime;
+    
+}
