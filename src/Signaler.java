2babdcb7bb4430ce19ef54fea0a0a86ccd183ad9
ColdAnkles <norealname8@hotmail.com>
2015-09-05 16:50:07 +0930

    Changed Signaler

--- src/Signaler.java
+++ src/Signaler.java
@@ -1,3 +1,8 @@
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,16 +16,27 @@
 public class Signaler {
     
     private int sigNum=0;
+    ArrayList<Thread> threadQueue = new ArrayList<Thread>();
+    
     
     public void Wait(){
 	sigNum--;
-	while(sigNum<=0){
-	    //noop
+	if(sigNum<=0){
+	    threadQueue.add(Thread.currentThread());
+	    try {
+		Thread.currentThread().wait();
+	    } catch (InterruptedException ex) {
+		Logger.getLogger(Signaler.class.getName()).log(Level.SEVERE, null, ex);
+	    }
 	}
     }
     
     public int Signal(){
 	sigNum++;
+	if(sigNum<=0){
+	    threadQueue.get(0).notify();
+	    threadQueue.remove(0);
+	}
 	return sigNum;
     }
     
